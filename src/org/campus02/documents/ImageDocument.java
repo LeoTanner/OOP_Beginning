42f4bad8827f98b996eff5f2037ce41d6b4f84d9
skochauf <stephan.kochauf@lv.campus02.at>
2021-12-03 20:57:59 +0100

    ImageDocument

---
+++ src/org/campus02/documents/ImageDocument.java
@@ -0,0 +1,24 @@
+package org.campus02.documents;
+
+// ALT+ENTER
+public class ImageDocument extends Document{
+
+    private int sizeX;
+    private int sizeY;
+    private String colour;
+
+    public ImageDocument(String filename, int sizeX, int sizeY, String colour) {
+        super(filename);
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.colour = colour;
+    }
+
+    @Override
+    public void printDocument() {
+        System.out.println("Filename: " + filename);
+        System.out.println("SizeX: " + sizeX);
+        System.out.println("SizeY: " + sizeY); // STRG+D
+        System.out.println("Colour: " + colour); // STRG+Cursor/links/rechts
+    }
+}
